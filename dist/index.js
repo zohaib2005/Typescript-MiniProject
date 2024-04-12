4fe2318795185a9a0fcf423c7bfa00d4c4cbc8fc
zohaib <zohaibshafiq2005@gmail.com>
2024-04-12 16:07:07 +0500

    typescript non null assertion operator

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("I AM HUNGRY!!!!!!!!!");
+const btn = document.getElementById("btn");
+// By default typescript will assume btn type will be HTMLElement | null so one solution is to use ?
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
+    alert("CLICKED!!!");
+});
+// We can also use Typescript Non Null Assertion Operator !
+// with TypeScript Non Null Assertion Operator
+// It will tell TypeScript don't worry we assure btn will not be null
+const bton = document.getElementById("btn");
+bton.addEventListener("click", function () {
+    alert("CLICKED!!!");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("I AM HUNGRY!!!!!!!!!");
+const btn = document.getElementById("btn");
+// By default typescript will assume btn type will be HTMLElement | null so one solution is to use ?
+
+btn?.addEventListener("click", function () {
+  alert("CLICKED!!!");
+});
+
+// We can also use Typescript Non Null Assertion Operator !
+// with TypeScript Non Null Assertion Operator
+// It will tell TypeScript don't worry we assure btn will not be null
+const bton = document.getElementById("btn")!;
+
+bton.addEventListener("click", function () {
+  alert("CLICKED!!!");
+});
