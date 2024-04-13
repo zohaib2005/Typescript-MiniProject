b2d7fbe8db4c563940f2e56c3a016428c13faf9d
zohaib <zohaibshafiq2005@gmail.com>
2024-04-13 11:08:17 +0500

    type assertions provide specific element type htmlInputElement not generic htmlElement

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,14 @@ btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function
 // We can also use Typescript Non Null Assertion Operator !
 // with TypeScript Non Null Assertion Operator
 // It will tell TypeScript don't worry we assure btn will not be null
+// Type Assertions -> button and input will have HTMLElement as parent class by default, but to make specific type we use as keyword
+// and specify HTMLButtonElement or HTMLInputElement
 const bton = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+input;
 bton.addEventListener("click", function () {
-    alert("CLICKED!!!");
+    alert(input.value);
+    input.value = "";
 });
+let mystery = "Hello World!!!";
+const numChars = mystery.length;
--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,17 @@ btn?.addEventListener("click", function () {
 // We can also use Typescript Non Null Assertion Operator !
 // with TypeScript Non Null Assertion Operator
 // It will tell TypeScript don't worry we assure btn will not be null
-const bton = document.getElementById("btn")!;
+// Type Assertions -> button and input will have HTMLElement as parent class by default, but to make specific type we use as keyword
+// and specify HTMLButtonElement or HTMLInputElement
+const bton = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+input;
 
 bton.addEventListener("click", function () {
-  alert("CLICKED!!!");
+  alert(input.value);
+  input.value = "";
 });
+
+let mystery: unknown = "Hello World!!!";
+
+const numChars = (mystery as string).length;
